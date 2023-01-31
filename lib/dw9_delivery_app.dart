d673eda7f502884b3fbaac393f106dbd02d2768b
rudarabello <ruda.rabello.silva@gmail.com>
2023-01-31 15:39:48 -0300

    feat: add router and main files

---
+++ lib/dw9_delivery_app.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Dw9DeliveryApp extends StatelessWidget {
+  const Dw9DeliveryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Delivery App",
+      routes: {
+        '/': (context) => Container(),
+      },
+    );
+  }
+}

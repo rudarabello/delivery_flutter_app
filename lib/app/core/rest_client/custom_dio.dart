5cccb7120edecf3c9ce08ed44a2268bd13561fa9
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-01 17:06:42 -0300

    core: add size helper

---
+++ lib/app/core/rest_client/custom_dio.dart
@@ -0,0 +1,27 @@
+import 'package:dio/dio.dart';
+import 'package:dio/native_imp.dart';
+import 'package:flutter/foundation.dart';
+
+import '../config/env/env.dart';
+
+class CustomDio extends DioForNative {
+  CustomDio()
+      : super(BaseOptions(
+            baseUrl: Env.i['backend_base_url'] ?? '',
+            connectTimeout: 5000,
+            receiveTimeout: 60000)) {
+    interceptors.add(LogInterceptor(
+      requestBody: true,
+      responseBody: true,
+      requestHeader: true,
+    ));
+  }
+
+  CustomDio auth() {
+    return this;
+  }
+
+  CustomDio unauth() {
+    return this;
+  }
+}

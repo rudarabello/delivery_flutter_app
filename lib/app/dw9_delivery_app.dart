0a16849ed62ead3bdf28aeb09888015463e4922d
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-01 16:06:20 -0300

    feat: add themeconfig

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -1,3 +1,4 @@
+import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,7 @@ class Dw9DeliveryApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Delivery App",
+      theme: ThemeConfig.theme,
       routes: {
         '/': (context) => const SplashPage(),
       },

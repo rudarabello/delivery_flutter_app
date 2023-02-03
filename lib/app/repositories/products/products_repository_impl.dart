3779ad16a4452701b504a3562071eca34ef5cdc3
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-03 09:50:43 -0300

    feat: add products repository

---
+++ lib/app/repositories/products/products_repository_impl.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+import '../../core/exceptions/repository_exception.dart';
+import '../../core/rest_client/custom_dio.dart';
+import '../../models/product_model.dart';
+import 'products_repository.dart';
+
+class ProductsRepositoryImpl implements ProductsRepository {
+  final CustomDio _dio;
+
+  const ProductsRepositoryImpl({required CustomDio dio}) : _dio = dio;
+
+  @override
+  Future<List<ProductModel>> findAllProducts() async {
+    try {
+      final response = await _dio.unauth().get('/products');
+
+      return (response.data as List)
+          .cast<Map<String, dynamic>>()
+          .map<ProductModel>(ProductModel.fromMap)
+          .toList();
+    } on DioError catch (e, s) {
+      log('Erro ao buscar produtos', error: e, stackTrace: s);
+      throw const RepositoryException(message: 'Erro ao buscar produtos');
+    }
+  }
+}

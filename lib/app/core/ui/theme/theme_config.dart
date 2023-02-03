eae3e560fc7add99678aa18a579474ce9637224f
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-03 18:19:21 -0300

    feat: button increment decrement

--- lib/app/core/ui/theme/theme_config.dart
+++ lib/app/core/ui/theme/theme_config.dart
@@ -1,46 +1,39 @@
 import 'package:flutter/material.dart';
 
+import '../styles/app_colors.dart';
 import '../styles/app_styles.dart';
-import '../styles/colors_app.dart';
 import '../styles/text_styles.dart';
 
-class ThemeConfig {
-  ThemeConfig._();
+final _defaultInputBorder = OutlineInputBorder(
+  borderRadius: const BorderRadius.all(Radius.circular(7)),
+  borderSide: BorderSide(color: Colors.grey[400]!),
+);
 
-  static final _defaultInputBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.circular(7),
-    borderSide: BorderSide(color: Colors.grey[400]!),
-  );
-
-  static final theme = ThemeData(
-    scaffoldBackgroundColor: Colors.white,
-    appBarTheme: const AppBarTheme(
-      backgroundColor: Colors.white,
-      elevation: 0,
-      centerTitle: true,
-      iconTheme: IconThemeData(
-        color: Colors.black,
-      ),
-    ),
-    primaryColor: ColorsApp.instance.primary,
-    colorScheme: ColorScheme.fromSeed(
-      seedColor: ColorsApp.instance.primary,
-      primary: ColorsApp.instance.primary,
-      secondary: ColorsApp.instance.secondary,
-    ),
-    elevatedButtonTheme: ElevatedButtonThemeData(
-      style: AppStyles.instance.primaryButton,
-    ),
-    inputDecorationTheme: InputDecorationTheme(
-      fillColor: Colors.white,
-      filled: true,
-      isDense: true,
-      contentPadding: const EdgeInsets.all(13),
-      border: _defaultInputBorder,
-      enabledBorder: _defaultInputBorder,
-      focusedBorder: _defaultInputBorder,
-      labelStyle: TextStyles.instance.textRegular.copyWith(color: Colors.black),
-      errorStyle: TextStyles.instance.textRegular.copyWith(color: Colors.red),
-    ),
-  );
-}
+final theme = ThemeData(
+  primaryColor: AppColors.i.primary,
+  colorScheme: ColorScheme.fromSeed(
+    seedColor: AppColors.i.primary,
+    primary: AppColors.i.primary,
+    secondary: AppColors.i.secondary,
+  ),
+  elevatedButtonTheme:
+      ElevatedButtonThemeData(style: AppStyles.i.primaryButton),
+  inputDecorationTheme: InputDecorationTheme(
+    fillColor: Colors.white,
+    filled: true,
+    isDense: true,
+    contentPadding: const EdgeInsets.all(13),
+    border: _defaultInputBorder,
+    enabledBorder: _defaultInputBorder,
+    focusedBorder: _defaultInputBorder,
+    labelStyle: TextStyles.i.textRegular.copyWith(color: Colors.black),
+    errorStyle: TextStyles.i.textRegular.copyWith(color: Colors.redAccent),
+  ),
+  scaffoldBackgroundColor: Colors.white,
+  appBarTheme: const AppBarTheme(
+    backgroundColor: Colors.white,
+    elevation: 0,
+    centerTitle: true,
+    iconTheme: IconThemeData(color: Colors.black),
+  ),
+);

0cd84e27b55ff340a6dd85ae9b56e9ba0b4a52b0
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-03 09:51:13 -0300

    feat: add splash page

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,32 +1,44 @@
-import 'package:dw9_delivery_app/app/core/config/env/env.dart';
-import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
-import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
-import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
+
+import '../../core/ui/helpers/size_extensions.dart';
+import '../../core/ui/widgets/delivery_button.dart';
 
 class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    context.colors.primary;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Splash'),
-      ),
-      body: Column(
-        children: [
-          Container(),
-          DeliveryButton(
-            width: 200,
-            height: 50,
-            label: Env.i['backend_base_url'] ?? '',
-            onPressed: () {},
-          ),
-          TextFormField(
-            decoration: InputDecoration(labelText: "text"),
-          )
-        ],
+      body: ColoredBox(
+        color: const Color(0XFF140E0E),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: SizedBox(
+                width: context.width,
+                child:
+                    Image.asset('assets/images/lanche.png', fit: BoxFit.cover),
+              ),
+            ),
+            Center(
+              child: Column(
+                children: [
+                  SizedBox(height: context.percentHeight(0.3)),
+                  Image.asset('assets/images/logo.png'),
+                  const SizedBox(height: 80),
+                  DeliveryButton(
+                    label: 'ACESSAR',
+                    width: context.percentWidth(0.6),
+                    height: 35,
+                    onPressed: () =>
+                        Navigator.of(context).popAndPushNamed('/home'),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }

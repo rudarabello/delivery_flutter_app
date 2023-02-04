55974f4889d898ff8e6d2eb576fc50df179b325a
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-03 21:35:45 -0300

    feat: add routes

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -4,6 +4,9 @@ import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
 import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
+import 'pages/auth/login/login_router.dart';
+import 'pages/auth/register/register_router.dart';
+
 class Dw9DeliveryApp extends StatelessWidget {
   const Dw9DeliveryApp({super.key});
 
@@ -12,10 +15,12 @@ class Dw9DeliveryApp extends StatelessWidget {
     return ApplicationBinding(
       child: MaterialApp(
         title: "Delivery App",
-        theme: ThemeConfig.theme,
+        theme: theme,
         routes: {
           '/': (context) => const SplashPage(),
           '/home': (context) => const HomeRouter(),
+          '/auth/login': (_) => const LoginRouter(),
+          '/auth/register': (_) => const RegisterRouter(),
         },
       ),
     );

49ebb64405efb0f02cb549440e7e99c8098bb6ee
rudarabello <ruda.rabello.silva@gmail.com>
2023-02-05 16:46:54 -0300

    feat: add routes to app

--- lib/app/dw9_delivery_app.dart
+++ lib/app/dw9_delivery_app.dart
@@ -1,26 +1,38 @@
-import 'package:dw9_delivery_app/app/core/provider/application_binding.dart';
-import 'package:dw9_delivery_app/app/core/ui/theme/theme_config.dart';
-import 'package:dw9_delivery_app/app/pages/home/home_router.dart';
-import 'package:dw9_delivery_app/app/pages/splash/splash_page.dart';
 import 'package:flutter/material.dart';
 
+import 'core/global/global_context.dart';
+import 'core/provider/application_binding.dart';
+import 'core/ui/theme/theme_config.dart';
 import 'pages/auth/login/login_router.dart';
 import 'pages/auth/register/register_router.dart';
+import 'pages/home/home_router.dart';
+import 'pages/order/order_completed_page.dart';
+import 'pages/order/order_router.dart';
+import 'pages/product_detail/product_detail_router.dart';
+import 'pages/splash/splash_page.dart';
 
-class Dw9DeliveryApp extends StatelessWidget {
-  const Dw9DeliveryApp({super.key});
+class DwDeliveryApp extends StatelessWidget {
+  final _navigatorKey = GlobalKey<NavigatorState>();
+
+  DwDeliveryApp({super.key}) {
+    GlobalContext.i.navigatorKey = _navigatorKey;
+  }
 
   @override
   Widget build(BuildContext context) {
     return ApplicationBinding(
       child: MaterialApp(
-        title: "Delivery App",
         theme: theme,
+        navigatorKey: _navigatorKey,
+        title: 'Delivery App',
         routes: {
-          '/': (context) => const SplashPage(),
-          '/home': (context) => const HomeRouter(),
+          '/': (_) => const SplashPage(),
+          '/home': (_) => const HomeRouter(),
+          '/product_detail': (_) => const ProductDetailRouter(),
           '/auth/login': (_) => const LoginRouter(),
           '/auth/register': (_) => const RegisterRouter(),
+          '/order': (_) => const OrderRouter(),
+          '/order/completed': (_) => const OrderCompletedPage(),
         },
       ),
     );
